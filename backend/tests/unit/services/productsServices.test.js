171bd97da1069cee07803a97533498ae3724d8b8
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-11-27 12:17:47 -0300

    fix

--- backend/tests/unit/services/productsServices.test.js
+++ backend/tests/unit/services/productsServices.test.js
@@ -4,7 +4,7 @@ const sinonChai = require('sinon-chai');
 const serviceIndex = require('../../../src/services/index');
 const productsService = require('../../../src/services/products.service');
 const productsModel = require('../../../src/models/products.model');
-const { getAllProductsDB, getAllProductsService } = require('../mocks/product.mock');
+const { getAllProductsDB } = require('../mocks/product.mock');
 
 chai.use(sinonChai);
 const { expect } = chai;
@@ -21,6 +21,7 @@ describe('Testa products da camada service', function () {
   it('Deve retornar todos os produtos com status SUCCESSFUL', async function () {
     sinon.stub(productsModel, 'getAllProducts').resolves(getAllProductsDB);
     const result = await productsService.getAllProducts();
+    
     expect(result).to.be.an('object');
     expect(result.status).to.be.equal('SUCCESSFUL');
     expect(result.data).to.deep.equal(getAllProductsDB);
